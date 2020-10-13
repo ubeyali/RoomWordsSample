789431e51881902645d68cbcdb4e45ae2ef64b64
Ubey <ubeyalim@gmail.com>
2020-10-13 13:17:22 +0300

    layouts

--- app/src/main/java/com/ubey/roomwordssample/Word.java
+++ app/src/main/java/com/ubey/roomwordssample/Word.java
@@ -5,17 +5,15 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
-@Entity (tableName = "word_table")
+@Entity(tableName = "word_table")
 public class Word {
 
     @PrimaryKey
     @NonNull
     @ColumnInfo(name = "word")
     private String mWord;
-    public Word(@NonNull String word) {
-        this.mWord = word;
-    }
-    public String getmWord(){
-        return this.mWord;
-    }
+
+    public Word(@NonNull String word) {this.mWord = word;}
+
+    public String getWord(){return this.mWord;}
 }
