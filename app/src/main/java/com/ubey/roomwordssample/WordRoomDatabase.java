716d8ce1e5a5884ad2627c519f1f893b5d7c4873
Ubey <ubeyalim@gmail.com>
2020-10-13 14:06:09 +0300

    connect ui with data

--- app/src/main/java/com/ubey/roomwordssample/WordRoomDatabase.java
+++ app/src/main/java/com/ubey/roomwordssample/WordRoomDatabase.java
@@ -1,10 +1,13 @@
 package com.ubey.roomwordssample;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities = {Word.class}, version =1, exportSchema =  false)
 public abstract class WordRoomDatabase extends RoomDatabase {
@@ -20,10 +23,41 @@ public abstract class WordRoomDatabase extends RoomDatabase {
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
                             WordRoomDatabase.class, "word_database")
                             .fallbackToDestructiveMigration()
+                            .addCallback(sRoomDatabaseCallback)
                             .build();
                 }
             }
         }
         return INSTANCE;
+    }
+    private static RoomDatabase.Callback sRoomDatabaseCallback =
+            new RoomDatabase.Callback(){
+
+            @Override
+            public void onOpen(@NonNull SupportSQLiteDatabase db) {
+                super.onOpen(db);
+                new PopulateDbAsync(INSTANCE).execute();
+
+            }
+        };
+    private static class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
+
+        private final WordDao mDao;
+        String[] words = {"dolphin", "crocodile", "cobra"};
+
+        PopulateDbAsync(WordRoomDatabase db) {
+            mDao = db.wordDao();
+        }
+
+        @Override
+        protected Void doInBackground(final Void... params) {
+            mDao.deleteAll();
+
+            for (int i = 0; i <= words.length - 1; i++) {
+                Word word = new Word(words[i]);
+                mDao.insert(word);
+            }
+            return null;
+        }
     }
 }
